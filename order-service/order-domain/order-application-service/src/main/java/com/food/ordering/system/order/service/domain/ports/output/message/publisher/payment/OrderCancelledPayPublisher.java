95e36803e4b91677bd2ae65b53d05a9bbfaa17ef
sendizakaria <zakaria_sendi@hotmail.com>
2023-11-12 19:15:34 +0000

    publish without event listener

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCancelledPayPublisher.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCancelledPayPublisher.java
@@ -3,5 +3,5 @@ package com.food.ordering.system.order.service.domain.ports.output.message.publi
 import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
 import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 
-public interface OrderCancelledPayPublisher extends DomainEventPublisher<OrderCancelledEvent> {
+public interface OrderCancelledPaymentRequestMessagePublisher extends DomainEventPublisher<OrderCancelledEvent> {
 }
--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCreatedPayPublisher.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCreatedPayPublisher.java
@@ -3,5 +3,5 @@ package com.food.ordering.system.order.service.domain.ports.output.message.publi
 import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 
-public interface OrderCreatedPayPublisher extends DomainEventPublisher<OrderCreatedEvent> {
+public interface OrderCreatedPaymentRequestMessagePublisher extends DomainEventPublisher<OrderCreatedEvent> {
 }
